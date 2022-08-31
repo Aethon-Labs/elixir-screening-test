a7da5df0c477a1b04b930dee40307c3d8d1c08fc
Prem Prakash <prakash2ji@gmail.com>
2022-08-30 22:38:16 -0300

    add app module

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule ElixirInterviewStarter.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {ElixirInterviewStarter.App, []},
       extra_applications: [:logger]
     ]
   end
